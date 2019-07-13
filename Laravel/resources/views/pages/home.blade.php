6dbf8ea664b77df3d1010caa61aa5a36918c35cc
mitul <mitulp660@gmail.com>
2019-07-13 19:11:31 -0400

    Created home and working on layout and includes

---
+++ Laravel/resources/views/pages/home.blade.php
@@ -0,0 +1,14 @@
+@extends('lay.default')
+
+@section('content')
+
+        <div class="jumbotron">
+            <div class="container">
+                <h1 class="display-3">HW-Page Dispaying Message</h1>
+
+            </div>
+        </div>
+
+
+
+@endsection
